fd0c786b86f7850b1b171fe0ba108e01fc6b2281
louazzanimaryem <maryemlouazzani@gmail.com>
2026-02-04 21:29:11 +0100

    snake_case

---
+++ snake_case.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+int main(int argc, char **argv)
+{
+    int i = 0;
+    if(argc > 1)
+    {
+        while(argv[1][i] != '\0')
+        {
+            if(argv[1][i] == ' ')
+            {
+                i++;
+                write(1 ,"_", 1);
+            }
+              write(1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+}
